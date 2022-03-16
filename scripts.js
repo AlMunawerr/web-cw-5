df810244c34ea0373179fd7526fb0dc058e97d95
Abbas Almunawer <almunawerabbas@gmail.com>
2022-03-16 22:39:36 +0300

    cw5-3
    تفضل

--- scripts.js
+++ scripts.js
@@ -9,3 +9,18 @@ let budget = 20;
 console.log(budget > 30 && budget < 80);
 budget += 30;
 console.log(budget > 30 && budget < 80);
+
+let grade = prompt("What is your grade?");
+if (grade >= 90) {
+  console.log("قد حصلت على امتياز🥳");
+} else if (grade >= 80) {
+  console.log("🤩لقد حصلت على جيد جدا ");
+} else if (grade >= 70) {
+  console.log("🙂لقد حصلت على جيد");
+} else if (grade >= 60) {
+  console.log("😕لقد حصلت على مقبول");
+} else if (grade >= 50) {
+  console.log("☹️لقد حصلت على ضعيف ");
+} else {
+  console.log("💔راسب");
+}
