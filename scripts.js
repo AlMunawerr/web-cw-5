211d66a51a56e31dc1ac11b6faabd313136a493e
Abbas Almunawer <almunawerabbas@gmail.com>
2022-03-16 21:49:16 +0300

    cw5-2
    here you go anas

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 alert("Abbas");
 console.log(16);
 console.log("انا في الصف الحادي عشر");
-javascript;
 console.log(
   "%c This is yellow text on a blue background.",
   "color:yellow; background-color:blue"
 );
+let budget = 20;
+console.log(budget > 30 && budget < 80);
+budget += 30;
+console.log(budget > 30 && budget < 80);
