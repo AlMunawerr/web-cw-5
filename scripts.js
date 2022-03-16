0ff9e616043be34f30d49cbf078024d2210c124d
Abbas Almunawer <almunawerabbas@gmail.com>
2022-03-16 18:13:29 +0300

    cw5 1
    here you gooo with the bonnnuuus

---
+++ scripts.js
@@ -0,0 +1,8 @@
+alert("Abbas");
+console.log(16);
+console.log("انا في الصف الحادي عشر");
+javascript;
+console.log(
+  "%c This is yellow text on a blue background.",
+  "color:yellow; background-color:blue"
+);
